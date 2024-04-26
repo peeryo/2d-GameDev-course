c958ed39a9093e86969c75b618609ec790f596d6
Owen Peery <peeryo@sfusd.edu>
2024-04-26 13:13:14 -0700

    L3 P1 and P2 done

--- M06.looting_workbook/addons/gdquest_practice_framework/practice_solutions/L3.P1.hover_the_balloon/balloon_hover.gd
+++ M06.looting_workbook/addons/gdquest_practice_framework/practice_solutions/L3.P1.hover_the_balloon/balloon_hover.gd
@@ -5,8 +5,8 @@ extends Area2D
 
 
 func _ready() -> void:
-	mouse_entered.connect(_on_mouse_entered)  #
-	mouse_exited.connect(_on_mouse_exited)  #
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
 	set_outline_thickness(5.0)
 
 
@@ -16,8 +16,10 @@ func set_outline_thickness(new_thickness: float) -> void:
 
 
 func _on_mouse_entered() -> void:
-	set_outline_thickness(10.0)  # pass
+	#var tween := create_tween()
+	set_outline_thickness(10.0)
 
 
 func _on_mouse_exited() -> void:
-	set_outline_thickness(5.0)  # pass
+	#var tween := create_tween()
+	set_outline_thickness(5.0)
