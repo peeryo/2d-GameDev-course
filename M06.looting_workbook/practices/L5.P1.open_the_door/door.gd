fb9d034fed399eb3f48c95a8e6f71927aa63637a
[peeryo] <peeryo@sfusd.edu>
2024-05-01 21:15:10 -0700

    L5P1 L5P2 completed

--- M06.looting_workbook/practices/L5.P1.open_the_door/door.gd
+++ M06.looting_workbook/practices/L5.P1.open_the_door/door.gd
@@ -6,8 +6,17 @@ extends Area2D
 func _input_event(viewport: Node, event: InputEvent, shape_index: int):
 	# Complete this boolean expression to check if the input event is a left-mouse click.
 	var event_is_mouse_click: bool = (
-		false
+		#add this code to check if the left mouse button has been pressed 
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and 
+		event.is_pressed()
 	)
 
 	if event_is_mouse_click:
-		pass
+		#after defining the open function, call it here
+		open()
+
+#add this function, open the DoorOpen scene, and set anim to open		
+func open() -> void:
+	animation_player.play("open")
+	input_pickable = false
--- M06.looting_workbook/practices/L5.P2.pop_the_balloon/balloon_pop.gd
+++ M06.looting_workbook/practices/L5.P2.pop_the_balloon/balloon_pop.gd
@@ -5,7 +5,10 @@ func _input_event(viewport: Node, event: InputEvent, shape_index: int):
 	var event_is_mouse_release: bool = (
 		event is InputEventMouseButton
 		and event.button_index == MOUSE_BUTTON_LEFT
+		#add the and and use released instead of clicked
+		and event.is_released()
 	)
 
 	if event_is_mouse_release:
-		pass
+		#use queue free to pop the balloon
+		queue_free()
