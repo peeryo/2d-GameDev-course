20b1e670de825159eddd1b91735a76f1d4fd5c92
[peeryo] <peeryo@sfusd.edu>
2024-05-06 06:30:21 -0700

    updated comments
    I attempted to comment in the code and add numbers as to the steps needed to complete this hardest challenge

--- M06.looting_workbook/lessons/bird.gd
+++ M06.looting_workbook/lessons/bird.gd
@@ -1,4 +1,9 @@
 extends Node2D
+#1 create a bird scene, area2d, add 2 sprite2d nodes, 1 for bird 1 for shadow
+#rename Shadow, keep bird at Sprite2D
+# add a timer and call it WaitTimer, one shot, Wait Time 1
+
+#3 drag nodes here to create references
 #remember to hold CMD on a Mac to auto write onready vars
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var wait_timer: Timer = $WaitTimer
@@ -6,13 +11,16 @@ extends Node2D
 
 
 func _ready() -> void:
+	#4 set up randomness and timer actions
 	#alternate between waiting and hopping
 	wait_timer.wait_time = randf_range(1.0, 3.0)
 	wait_timer.one_shot = true
 	wait_timer.timeout.connect(_animate_one_hop)
 	wait_timer.start()
 	
+#2 create an animate one hop function and put pass until we write it
 func _animate_one_hop() -> void:
+	#5 Now write this function one block at a time
 	#allows us to calculate direction during hop
 	const HOP_DURATION := 0.25
 	const HALF_HOP_DURATION := HOP_DURATION / 2.0
@@ -25,14 +33,16 @@ func _animate_one_hop() -> void:
 	var tween := create_tween().set_parallel()
 	tween.tween_property(sprite_2d, "position:x", land_position.x, HOP_DURATION)
 	
-	#why just position not position:x?
+	#notice the position is not position:x?
 	tween.tween_property(shadow, "position", land_position, HOP_DURATION)
 	
 	#create animation to make it jump up and down
 	tween = create_tween()
+	#these set properties of animations, read in course
 	tween.set_trans(Tween.TRANS_QUAD)
 	tween.set_ease(Tween.EASE_OUT)
 	const JUMP_HEIGHT := 16.0
+	#sets the jump up and down
 	tween.tween_property(sprite_2d, "position:y", land_position.y - JUMP_HEIGHT, HALF_HOP_DURATION)
 	tween.set_ease(Tween.EASE_IN)
 	tween.tween_property(sprite_2d, "position:y", land_position.y, HALF_HOP_DURATION)
