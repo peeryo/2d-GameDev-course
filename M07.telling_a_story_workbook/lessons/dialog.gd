60a3f527a7e16d14d0c059c6c755ad77029c764b
[peeryo] <peeryo@sfusd.edu>
2024-08-02 08:10:52 -0700

    M7L5 and practices all complete, including challenges

--- M07.telling_a_story_workbook/lessons/dialog.gd
+++ M07.telling_a_story_workbook/lessons/dialog.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var rich_text_label = %RichTextLabel
 @onready var next_button = %NextButton
+@onready var back_button = %BackButton
 
 var dialog_items :Array[String] = [
 	"Simple arrays are pretty easy",
@@ -19,13 +20,24 @@ func show_text() -> void:
 func _ready() -> void:
 	show_text()
 	next_button.pressed.connect(advance)
+	#I imagine you need a signal like this to rewind
+	back_button.pressed.connect(rewind)
 	
 func advance() -> void:
 	current_item_index += 1
-	if current_item_index == dialog_items.size():
-		get_tree().quit()
-	else:
-		show_text()
+	if current_item_index >= dialog_items.size(): #== also works, but >= makes more sense to display last line
+		#get_tree().quit()
+		current_item_index = 0
+	#else:
+	show_text()
+		
+#Here's the rewind function, not sure what should happen at 0		
+func rewind() -> void:
+	current_item_index -= 1
+	if current_item_index < 0:
+		current_item_index = 0
+	show_text()
+
 	
 
 	
