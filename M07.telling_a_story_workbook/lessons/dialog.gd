b7af21e0332a5ae12fe5b3d74aa890164beeffc0
[peeryo] <peeryo@sfusd.edu>
2024-08-07 12:49:44 -0700

    M7L6 done, practices and Challenge 1, not 2

--- M07.telling_a_story_workbook/lessons/dialog.gd
+++ M07.telling_a_story_workbook/lessons/dialog.gd
@@ -1,8 +1,11 @@
 extends Control
 
-@onready var rich_text_label = %RichTextLabel
-@onready var next_button = %NextButton
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+@onready var next_button: Button = %NextButton
 @onready var back_button = %BackButton
+@onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+
+
 
 var dialog_items :Array[String] = [
 	"Simple arrays are pretty easy",
@@ -16,6 +19,14 @@ var current_item_index := 0
 func show_text() -> void:
 	var current_item := dialog_items[current_item_index]
 	rich_text_label.text = current_item
+	rich_text_label.visible_ratio = 0.0
+	var tween := create_tween()
+	var text_appearing_duration := 1.2
+	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
+	var sound_max_length :=  audio_stream_player.stream.get_length() - text_appearing_duration
+	var sound_start_position := randf() * sound_max_length
+	audio_stream_player.play(sound_start_position)
+	tween.finished.connect(audio_stream_player.stop)
 	
 func _ready() -> void:
 	show_text()
