f70fdc7a38b8613015c6cfeea4065f79324c82df
[peeryo] <peeryo@sfusd.edu>
2024-10-27 11:28:31 -0700

    M7L11 completed and practices

--- M07.telling_a_story_workbook/lessons/dialog.gd
+++ M07.telling_a_story_workbook/lessons/dialog.gd
@@ -1,12 +1,4 @@
 extends Control
-
-@onready var rich_text_label: RichTextLabel = %RichTextLabel
-@onready var next_button: Button = %NextButton
-@onready var back_button = %BackButton
-@onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
-@onready var body: TextureRect = %Body
-@onready var expression: TextureRect = %Expression
-
 var expressions := {
 	"happy": preload("res://assets/emotion_happy.png"),
 	"regular": preload("res://assets/emotion_regular.png"),
@@ -34,32 +26,47 @@ var dialog_items :Array[Dictionary] = [
 
 var current_item_index := 0
 
-func show_text() -> void:
-	var current_item := dialog_items[current_item_index]
-	rich_text_label.text = current_item["text"]
-	expression.texture = current_item["expression"]
-	rich_text_label.visible_ratio = 0.0
-	var tween := create_tween()
-	var text_appearing_duration := 1.2
-	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
-	var sound_max_length :=  audio_stream_player.stream.get_length() - text_appearing_duration
-	var sound_start_position := randf() * sound_max_length
-	audio_stream_player.play(sound_start_position)
-	tween.finished.connect(audio_stream_player.stop)
-	
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+@onready var next_button: Button = %NextButton
+@onready var back_button = %BackButton
+@onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+@onready var body: TextureRect = %Body
+@onready var expression: TextureRect = %Expression
+
 func _ready() -> void:
 	show_text()
 	next_button.pressed.connect(advance)
 	#I imagine you need a signal like this to rewind
 	back_button.pressed.connect(rewind)
+
+## Draws the current text to the rich text element
+func show_text() -> void:
+	# We retrieve the current dictionary from the array and assign its
+	# properties to the UI elements.
+	var current_item := dialog_items[current_item_index]
+	rich_text_label.text = current_item["text"]
+	expression.texture = current_item["expression"]
+	# We animate the text appearing letter by letter.
+	rich_text_label.visible_ratio = 0.0
+	var tween := create_tween()
+	var text_appearing_duration := 1.0
+	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
+
+	# This is where we play the audio. We randomize the audio playback's start
+	# time to make it sound different every time.
+	var sound_max_length := audio_stream_player.stream.get_length() - text_appearing_duration
+	var sound_start_position := randf() * sound_max_length
+	audio_stream_player.play(sound_start_position)
+	# We stop the audio when the text finishes appearing.
+	tween.finished.connect(audio_stream_player.stop)
+	slide_in()
 	
 func advance() -> void:
 	current_item_index += 1
-	if current_item_index >= dialog_items.size(): #== also works, but >= makes more sense to display last line
-		#get_tree().quit()
+	if current_item_index >= dialog_items.size():
 		current_item_index = 0
-	#else:
-	show_text()
+	else:
+		show_text()
 		
 #Here's the rewind function, not sure what should happen at 0		
 func rewind() -> void:
@@ -67,6 +74,16 @@ func rewind() -> void:
 	if current_item_index < 0:
 		current_item_index = 0
 	show_text()
+	
+func slide_in() -> void:
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_QUART)
+	tween.set_ease(Tween.EASE_OUT)
+	body.position.x = 200.0
+	tween.tween_property(body, "position:x", 0.0, 0.3)
+	body.modulate.a = 0.0
+	tween.parallel().tween_property(body, "modulate:a", 1.0, 0.2)
+	
 
 	
 
